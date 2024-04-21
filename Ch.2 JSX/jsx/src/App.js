4b8cfa2bff679bf673adf53a090d2516b132c380
DESKTOP-3IVCKQQ\lkas7 <lkas78@naver.com>
2024-04-21 15:56:03 +0900

    Ch.2 JSX
       # JSX 기본 문법 ( 조건연산자, &&, || 등 )
       # Inline Styling
       # className
       # ESLint, Prettier

---
+++ Ch.2 JSX/jsx/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    <div>abc</div>
+    // <Fragment>abc</Fragment>
+  );
+}
+
+export default App;
