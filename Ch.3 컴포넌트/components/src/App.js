e3a5d7a3e5bb6abc04599b76ef8aa2eae1ad7948
DESKTOP-3IVCKQQ\lkas7 <lkas78@naver.com>
2025-01-28 17:37:20 +0900

    Ch.3 컴포넌트
       # 컴포넌트 유형 ( 클래스, 함수형 )
       # props, state
       # 함수형 컴포넌트에서 PropTypes 은 지원하지 않음 (v15.5.0)
       # setState, useState
       # state 주의사항
           - setState, useState Setter 를 통해서만 state 값을 변경해야 함
           - Object, Array 의 경우, 새로운 객체로 생성해서 매핑해주어야 한다. ( ..., concat(), filter(), map() 등 )

---
+++ Ch.3 컴포넌트/components/src/App.js
@@ -0,0 +1,13 @@
+import MyComponent from './MyComponent';
+import Say from './Say';
+
+function App() {
+  return (
+    <div>
+      {/* <MyComponent /> */}
+      <Say/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Ch.3 컴포넌트/components/src/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyComponent = (props) => {
+    return (
+        <div>
+            {props.name} Component
+        </div>
+    );
+};
+
+export default MyComponent;
